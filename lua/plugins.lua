7ff7af0dc3c3f41f10fdb10d3ea48fd90d1f57ad
Francisco Sánchez <ing.fran12@gmail.com>
2023-07-02 13:31:20 -0700

    Clean ups

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,11 @@
 return {
-  -- 'morhetz/gruvbox',
   { "ellisonleao/gruvbox.nvim", priority = 1000 },
 
-  -- Clojure
-  'Olical/aniseed',
-  'bakpakin/fennel.vim',
-  'Olical/conjure',
   'm00qek/baleia.nvim',
   'luochen1990/rainbow',
   'tpope/vim-sexp-mappings-for-regular-people',
   'guns/vim-sexp',
 
-  'dart-lang/dart-vim-plugin',
   'tpope/vim-fugitive',
   'airblade/vim-gitgutter',
   'stephpy/vim-yaml',
@@ -20,7 +14,4 @@ return {
   'mhinz/vim-sayonara',
   'junegunn/vim-easy-align',
   'ruanyl/vim-gh-line',
-
-  -- Copilot
-  -- 'github/copilot.vim'
 }
