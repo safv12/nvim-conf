fc22d4e13b171a209f25df191637a4a25349b26b
Francisco Sanchez <francisco.sanchez@nubank.com.br>
2023-09-22 09:35:23 -0600

    Neotree keys

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,6 +8,8 @@ return{
     },
   keys = {
     { "<C-n>", "<cmd>Neotree filesystem toggle<CR>", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<localleader>f", "<cmd>Neotree reveal<CR>", desc = "Reveal current file", remap = true },
     { "<localleader>gst", "<cmd>Neotree git_status<CR>", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>b", "<cmd>Neotree toggle show buffers right<CR>", desc = "Explore buffers", remap = true },
   }
 }
