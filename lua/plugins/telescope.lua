18080be34d6a2aeabf652414628cab121fac6ee9
Francisco Sanchez <francisco.sanchez@nubank.com.br>
2024-08-20 15:53:55 -0700

    Gst telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
         require("telescope")
 
         local opts = { noremap = true, silent = true }
+        vim.keymap.set('n', '<leader>gst', "<cmd>Telescope git_status<CR>", {})
         vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
         vim.keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<CR>", opts)
         vim.keymap.set("n", "<C-b>", "<cmd>Telescope buffers<CR>", opts)
